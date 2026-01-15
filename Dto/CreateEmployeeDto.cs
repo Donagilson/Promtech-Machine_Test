63c14e20e50877d8ec0769f906e6485057dc41ce
dona <donagilson2002@gmail.com>
2026-01-15 14:08:29 +0530

    Initial commit - Promtech machine test

---
+++ Dto/CreateEmployeeDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Api.DTOs
+{
+    public class CreateEmployeeDto
+    {
+
+        // Name is required
+        // Maximum length  is 100 characters
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
+        public string Name { get; set; } = string.Empty;
+
+
+        // Email is mandatory
+        // Validates email format ,maximum length is 100 
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [MaxLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
+        public string Email { get; set; } = string.Empty;
+
+
+        // Department is mandatory
+        // Maximum length allowed is 50 characters
+        [Required(ErrorMessage = "Department is required")]
+        [MaxLength(50, ErrorMessage = "Department cannot exceed 50 characters")]
+        public string Department { get; set; } = string.Empty;
+
+
+        // Salary is mandatory
+        // Accepts values only between 0 and 999999.99
+        [Required(ErrorMessage = "Salary is required")]
+        [Range(0, 999999.99, ErrorMessage = "Salary must be between 0 and 999999.99")]
+        public decimal Salary { get; set; }
+    }
+}
---
+++ Migrations/20260115062312_INITIAL MIGRATION.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Promtech_Machine_Test.Migrations
+{
+    /// <inheritdoc />
+    public partial class INITIALMIGRATION : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Email",
+                table: "Employees",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
